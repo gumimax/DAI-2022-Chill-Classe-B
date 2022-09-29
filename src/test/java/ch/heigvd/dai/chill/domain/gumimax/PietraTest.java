67ac0a94bf72d60057604c0bbe99f32fc25d07a8
Guillaume Courbat <guillaume.courbat@heig-vd.ch>
2022-09-29 11:14:37 +0200

    missing files

--- src/test/java/ch/heigvd/dai/chill/domain/gumimax/PietraTest.java
+++ src/test/java/ch/heigvd/dai/chill/domain/gumimax/PietraTest.java
@@ -1,2 +1,36 @@
-package ch.heigvd.dai.chill.domain.gumimax;public class PietraTest {
+package ch.heigvd.dai.chill.domain.gumimax;
+
+import ch.heigvd.dai.chill.domain.Bartender;
+import ch.heigvd.dai.chill.protocol.OrderRequest;
+import ch.heigvd.dai.chill.protocol.OrderResponse;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class PietraTest {
+
+
+    @Test
+    void thePriceAndNameForPietraShouldBeCorrect() {
+        Pietra beer = new Pietra();
+        assertEquals(beer.getName(), Pietra.NAME);
+        assertEquals(beer.getPrice(), Pietra.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForPietra() {
+        Bartender jeanne = new Bartender();
+        String productName = "ch.heigvd.dai.chill.domain.gumimax.Pietra";
+        OrderRequest request = new OrderRequest(5, productName);
+        OrderResponse response = jeanne.order(request);
+        BigDecimal expectedTotalPrice = Pietra.PRICE.multiply(new BigDecimal(5));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
 }
+
+
